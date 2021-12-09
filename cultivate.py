20aecaddb5a0d432c8de260ac54b7771e06e8de3
Veer Sandhu <56776466+Real-VeerSandhu@users.noreply.github.com>
2021-12-09 09:09:49 -0500

    Update cultivate.py

--- cultivate.py
+++ cultivate.py
@@ -3,7 +3,7 @@ from random import randint
 def plant():
     crop = randint(10000, 99999) # Number Id
     
-    land = open('dump/land.txt', 'a')
+    land = open('land.txt', 'a')
     land.write(f'\n{crop}')
     land.close()
 
